e8f39e90652f1670581f3c312c21427c433885f3
EmyleSilva <emylesnt.silva@gmail.com>
2024-12-02 14:52:42 -0300

    Mudança na documentação de português para inglês

--- print_fdt_cat_default.py
+++ print_fdt_cat_default.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 def print_fdt_cat_default(x, columns=range(6), round=2, row_names=False, right=True, **kwargs):
-    # Concatena a linha 0 com as linhas 1 a 5 (arredondadas)
+    # Concatenate line 0 with lines 1 to 5 (rounded)
     res = pd.concat([x.iloc[:, [0]], x.iloc[:, 1:6].round(round)], axis=1)
-    # Filtra as colunas
+    # Filters the columns
     res = res.iloc[:, columns]
 
-    # Nomeia as colunas
+    # Name the columns
     columns_names = columns_names = ['Category', 'f', 'rf', 'rf(%)', 'cf', 'cf(%)']
     res.columns = [columns_names[i] for i in columns]
 
